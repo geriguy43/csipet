77b5c4fa05501d3abf205eac9768b054e27f50b2
geriguy43 <info@bestmachinery.hu>
2023-02-10 13:48:31 +0100

    Update _document.tsx

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Head, Main, NextScript } from "next/document";
-import Script from 'next/script'
 import { ServerStyleSheet } from "styled-components";
 import getConfig from "next/config";
 import React from "react";
@@ -27,11 +26,11 @@ class AppDocument extends Document<Props> {
     return (
       <html lang="hu">
         <Head>
-<Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W4K9L8S');`}}></Script>
+})(window,document,'script','dataLayer','GTM-W4K9L8S');`}}></script>
           <meta charSet="utf-8" />
           <meta
             name="viewport"
